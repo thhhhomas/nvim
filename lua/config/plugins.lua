2362d2518c5f0969d42e459125953c56eecb5fe9
thhhhomas <thomasferreiralopes6@gmail.com>
2025-05-26 19:57:13 -0300

    LSP

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -65,5 +65,73 @@ require("lazy").setup({
         default_detail = 1
       },
     },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("config.plugins.lsp")
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    build = ":MasonUpdate",
+    config = true,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim", "neovim/nvim-lspconfig" },
+    config = function()
+      require("mason").setup()
+      require("mason-lspconfig").setup {
+        ensure_installed = { "pyright" },
+      }
+
+      local lspconfig = require("lspconfig")
+      lspconfig.pyright.setup({})
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",     -- fonte de completions do LSP
+      "hrsh7th/cmp-buffer",       -- completar pelo conteúdo do buffer
+      "hrsh7th/cmp-path",         -- completar caminhos
+      "hrsh7th/cmp-cmdline",      -- completar comandos
+      "L3MON4D3/LuaSnip",         -- snippet engine
+      "saadparwaiz1/cmp_luasnip", -- snippet completions
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+        }, {
+          { name = "buffer" },
+        })
+      })
+
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "path" },
+          { name = "cmdline" }
+        }
+      })
+    end,
   }
 })
---
+++ lua/config/plugins/lsp.lua
@@ -0,0 +1,16 @@
+local on_attach = function(_, bufnr)
+  local opts = { noremap=true, silent=true, buffer=bufnr }
+
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+end
+
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+require("lspconfig").pyright.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+})
