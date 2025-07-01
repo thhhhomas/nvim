94b3f7d552d4770eab75520a74061a85dbe37c20
thhhhomas <thomasferreiralopes6@gmail.com>
2025-06-30 21:31:29 -0300

    Configurando OmniSharp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,73 +7,59 @@ return {
       'hrsh7th/cmp-nvim-lsp',
     },
     config = function()
+      local lspconfig = require("lspconfig")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      
+      
       local on_attach = function(_, bufnr)
         local map = vim.keymap.set
         local opts = { noremap = true, silent = true, buffer = bufnr }
 
-        map('n', 'K', vim.lsp.buf.hover, opts)
-        map('n', 'gd', vim.lsp.buf.definition, opts)
-        map('n', 'gr', vim.lsp.buf.references, opts)
-        map('n', '<leader>rn', vim.lsp.buf.rename, opts)
-        map('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+        map("n", "K", vim.lsp.buf.hover, opts)
+        map("n", "gd", vim.lsp.buf.definition, opts)
+        map("n", "gr", vim.lsp.buf.references, opts)
+        map("n", "<leader>rn", vim.lsp.buf.rename, opts)
+        map("n", "<leader>ca", vim.lsp.buf.code_action, opts)
       end
 
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      require("mason").setup()
 
-      require('mason').setup()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "pyright", "omnisharp" },
+      })
 
-      require('mason-lspconfig').setup {
-        ensure_installed = { 'lua_ls', 'pyright' },
-      }
+      lspconfig.lua_ls.setup({
+        on_attach = on_attach,
+        capabilities = capabilities,
+      })
 
-      local lspconfig = require('lspconfig')
+      lspconfig.pyright.setup({
+        on_attach = on_attach,
+        capabilities = capabilities,
+        settings = {
+          python = {
+            pythonPath = (function()
+              local venv_names = { "venv", "env", ".venv", ".env" }
+              for _, name in ipairs(venv_names) do
+                local python_path = vim.fn.getcwd() .. "/" .. name .. "/bin/python"
+                if vim.fn.executable(python_path) == 1 then
+                  return python_path
+                end
+              end
+              return "python"
+            end)(),
+          },
+        },
+      })
+      
+      path_omnisharp = os.getenv("HOME") .. "/.local/share/nvim/mason/packages/omnisharp/OmniSharp"
 
-      local servers = { 'lua_ls', 'pyright', 'omnisharp' }
-      local function get_python_path(workspace)
-        local venv_names = { 'venv', 'env', '.venv', '.env' }
-        for _, name in ipairs(venv_names) do
-          local python_path = workspace .. '/' .. name .. '/bin/python'
-          if vim.fn.executable(python_path) == 1 then
-            return python_path
-          end
-        end
-        return 'python'
-      end
+      lspconfig.omnisharp.setup({
+        cmd = { path_omnisharp, "--languageserver", "--hostPID", tostring(vim.fn.getpid()) },
+        on_attach = on_attach,
+        capabilities = capabilities,
+      })
 
-      for _, server in ipairs(servers) do
-        if server == 'pyright' then
-          lspconfig[server].setup {
-            on_attach = on_attach,
-            capabilities = capabilities,
-            
-            settings = {
-              python = {
-                pythonPath = get_python_path(vim.fn.getcwd()),
-                analysis = {
-                  useLibraryCodeForTypes = true,
-                  autoSearchPaths = true,
-                  diagnosticMode = 'workspace',
-                  typeCheckingMode = 'off',
-                }
-              }
-            }
-          }
-        elseif server == 'omnisharp' then
-          lspconfig[server].setup {
-            cmd = {'omnisharp', '--languageserver', '--hostPID', tostring(vim.fn.getpid()) },
-            capabilities = capabilities,
-          }
-
-        else
-          lspconfig[server].setup {
-            on_attach = on_attach,
-            capabilities = capabilities,
-            flags = {
-              debounce_text_changes = 150,
-            },
-          }
-        end
-      end
     end,
   },
 }
