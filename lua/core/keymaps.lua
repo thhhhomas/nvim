7e93b4acc094fddb739e37ab44b3f839e5fe40c1
thhhhomas <thomasferreiralopes6@gmail.com>
2025-07-06 21:39:02 -0300

    Configuração do Telescope para buffers e ativação/desativação do painel do copilot

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -4,17 +4,50 @@ local copilot_ativo = false
 
 des_ativar_copilot = function ()
   local copilot = require("copilot.suggestion")
+
   if  copilot_ativo then
     require("copilot.suggestion").dismiss()
     vim.cmd("Copilot disable") 
     vim.notify("Copilot desativado", vim.log.levels.INFO)
+  
   else
     vim.cmd("Copilot enable")
     vim.notify("Copilot ativado", vim.log.levels.INFO)
   end
+  
   copilot_ativo = not copilot_ativo
 end
 
 vim.keymap.set("n", "<leader>cp", des_ativar_copilot, { desc = "Ativar/Desativar Copilot" })
 
+-- Panel do Copilot
+
+local painel_aberto = false
+
+abrir_fechar_panel_copilot = function ()
+  if not copilot_ativo then
+    vim.notify("Copilot está desativado", vim.log.levels.INFO)
+    return
+  end
+
+  if not painel_aberto then
+    vim.cmd("Copilot panel")
+    painel_aberto = not painel_aberto
+    vim.notify("Painel do Copilot", vim.log.levels.INFO) 
+    return 
+  end 
+
+  require("copilot.panel").toggle() 
+  painel_aberto = not painel_aberto
+  vim.notify(" ", vim.log.levels.INFO)
+
+  return
+end
+
+vim.keymap.set("n", "<leader>pc", abrir_fechar_panel_copilot, { desc = "Abrir/Fechar painel do Copilot" })
+
 ---------------------------------------------------------------------------------------------
+
+-- Buffers pelo Telescope -------------------------------------------------------------------
+
+vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<CR>", { desc = "Abrir lista de buffers" })
