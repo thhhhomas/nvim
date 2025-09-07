55ee4c1e9b46f519f184d26898e3ba2efe94b3f4
thhhhomas <thomasferreiralopes6@gmail.com>
2025-09-07 08:48:14 -0300

    Atualização do venv selector

--- lua/plugins/venv-selector.lua
+++ lua/plugins/venv-selector.lua
@@ -1,16 +1,23 @@
 return {
-  'linux-cultist/venv-selector.nvim',
-  dependencies = { 'neovim/nvim-lspconfig', 'nvim-telescope/telescope.nvim', 'mfussenegger/nvim-dap-python' },
-  opts = {
-    -- Your options go here
-    name = {"venv", ".venv", "env", ".env"},
-    -- auto_refresh = false
+  "linux-cultist/venv-selector.nvim",
+  branch = "regexp",
+  dependencies = {
+    "neovim/nvim-lspconfig",
+    "nvim-telescope/telescope.nvim",
+    "mfussenegger/nvim-dap-python",
   },
-  --event = 'VeryLazy', -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
+  opts = {
+    search = {
+      venvs = {
+        command = "fd .venv$ .",
+      },
+      home_venvs = {
+        command = "fd python$ ~/venvs",
+      },
+    },
+  },
   keys = {
-    -- Keymap to open VenvSelector to pick a venv.
-    { '<leader>vs', '<cmd>VenvSelect<cr>' },
-    -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
-    { '<leader>vc', '<cmd>VenvSelectCached<cr>' },
+    { "<leader>vs", "<cmd>VenvSelect<cr>" },
+    { "<leader>vc", "<cmd>VenvSelectCached<cr>" },
   },
 }
