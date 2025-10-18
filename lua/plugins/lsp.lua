e029046d9f16dec1be4681966c2a833cd1da821c
thhhhomas <thomasferreiralopes6@gmail.com>
2025-10-18 09:54:02 -0300

    update do lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,59 +7,11 @@ return {
       'hrsh7th/cmp-nvim-lsp',
     },
     config = function()
-      local lspconfig = require("lspconfig")
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      
-      
-      local on_attach = function(_, bufnr)
-        local map = vim.keymap.set
-        local opts = { noremap = true, silent = true, buffer = bufnr }
-
-        map("n", "K", vim.lsp.buf.hover, opts)
-        map("n", "gd", vim.lsp.buf.definition, opts)
-        map("n", "gr", vim.lsp.buf.references, opts)
-        map("n", "<leader>rn", vim.lsp.buf.rename, opts)
-        map("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-      end
-
       require("mason").setup()
 
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "pyright", "omnisharp", "arduino_language_server", "clangd" },
+        ensure_installed = { "lua_ls", "pyright", "omnisharp", "clangd" },
       })
-
-      lspconfig.lua_ls.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-      })
-
-      lspconfig.pyright.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-        settings = {
-    	  python = {
-            analysis = {
-            autoSearchPaths = true,
-            useLibraryCodeForTypes = true,
-          },
-          pythonPath = vim.g["venv_selector_virtual_env"] or vim.fn.exepath("python3"),
-          },
-        },
-      })
-
-      lspconfig.clangd.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-      })
-      
-      path_omnisharp = os.getenv("HOME") .. "/.local/share/nvim/mason/packages/omnisharp/OmniSharp"
-
-      lspconfig.omnisharp.setup({
-        cmd = { path_omnisharp, "--languageserver", "--hostPID", tostring(vim.fn.getpid()) },
-        on_attach = on_attach,
-        capabilities = capabilities,
-      })
-      
     end,
   },
 }
