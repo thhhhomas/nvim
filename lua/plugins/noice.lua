2a382c07111c95fd5dad48bebd809e16c36585e1
thhhhomas <thomasferreiralopes6@gmail.com>
2025-11-12 21:07:11 -0300

    Revert "Arrumando noice"
    This reverts commit 593071390a433669cac5f1104c22d06b546b0e3e.

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,26 +1,14 @@
 return {
   'folke/noice.nvim',
   event = 'VeryLazy',
-  opts = {},
+  opts = {
+
+  },
   dependencies = {
     'MunifTanjim/nui.nvim',
     'rcarriga/nvim-notify',
   },
   config = function ()
-    require("noice").setup({
-      views = {
-        mini = {
-          win_options = {
-            winblend = 0,
-          }
-        }
-      }
-    })
-
-    require('notify').setup({
-      background_colour = "#000000",
-    })
-
-    vim.notify = require("notify")
+       
   end
 }
