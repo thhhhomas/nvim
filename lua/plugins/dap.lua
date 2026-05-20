0407bffada1fda388083ba0a9bd62b8cc978959b
thhhhomas <thomasferreiralopes6@gmail.com>
2026-05-20 11:35:04 -0300

    configuraçao do dap para csharp

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -7,18 +7,12 @@ return{
     "nvim-neotest/nvim-nio",
   },
   config = function()
-    local dap = require("dap")
-    local dapui = require("dapui")
-
-    require("mason-nvim-dap").setup({
-      ensure_installed = { "netcoredbg" },
-      handlers = {},
-    })
-
+    local dap, dapui = require("dap"), require("dapui")
+    
     dap.adapters.coreclr = {
-      type = "executable",
-      command = vim.fn.stdpath("data") .. "/mason/bin/netcoredbg",
-      args = { "--interpreter=vscode"},
+      type = 'executable',
+      command = '/usr/local/bin/netcoredbg',
+      args = {'--interpreter=vscode'}
     }
 
     dap.configurations.cs = {
@@ -26,28 +20,37 @@ return{
         type = "coreclr",
         name = "launch - netcoredbg",
         request = "launch",
-
         program = function()
-          return vim.fn.input("Path to dll: ", vim.fn.getcwd() .. '/bin/Debug', 'file')
+          return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/bin/Debug/', 'file')
         end,
       },
     }
 
-    vim.keymap.set("n", "<F2>", function() dap.continue() end)
-    vim.keymap.set("n", "<F3>", function() dap.step_over() end)
-    vim.keymap.set("n", "<F4>", function() dap.step_into() end)
-    vim.keymap.set("n", "<F5>", function() dap.step_out() end)
-    vim.keymap.set("n", "<leader>b", function() dap.toggle_breakpoint() end)
-    vim.keymap.set("n", "<leader>B", function() dap.set_breakpoint(vim.fn.input('Breakpoint condition: ')) end)
+    vim.keymap.set("n", "<F9>", dap.toggle_breakpoint)
 
-    dap.listeners.after.event_initialized["dapui_config"] = function()
+    vim.keymap.set("n", "<C-?>", function()
+      require("dapui").eval(nil, { enter = true })
+    end)
+
+    vim.keymap.set("n", "<F5>", dap.continue)
+    vim.keymap.set("n", "<F10>", dap.step_over)
+    vim.keymap.set("n", "<F11>", dap.step_into)
+    vim.keymap.set("n", "<S-F11>", dap.step_out)
+    vim.keymap.set("n", "<F10>", dap.step_over)
+    vim.keymap.set("n", "<F10>", dap.step_over)
+    vim.keymap.set("n", "<S-F5>", function ()
+      dap.terminate()
+      dapui.close()
+    end)
+    vim.keymap.set("n", "<C-S-F5>", dap.restart)
+
+    dap.listeners.before.attach.dapui_config = function ()
       dapui.open()
     end
-    dap.listeners.before.event_terminated["dapui_config"] = function()
-      dapui.close()
+
+    dap.listeners.before.launch.dapui_config = function ()
+      dapui.open()
     end
-    dap.listeners.before.event_exited["dapui_config"] = function()
-      dapui.close()
-    end
+    
   end,
 }
