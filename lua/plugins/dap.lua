a1162ae973672cd45cd7e7fd573e45a756665dcc
thhhhomas <thomasferreiralopes6@gmail.com>
2026-05-21 08:50:09 -0300

    debbuger para clang

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -24,10 +24,46 @@ return{
           local pathDebug = vim.fn.getcwd() .. '/bin/Debug/'
           local dirs = vim.fn.readdir(pathDebug)
 
-          return vim.fn.input('Path to dll', pathDebug .. dirs[1], 'file')
+          return vim.fn.input('Path to dll: ', pathDebug .. dirs[1], 'file')
         end,
       },
     }
+
+    dap.adapters.gdb = {
+      type = "executable",
+      command = "gdb",
+      args = { "--interpreter=dap", "--eval-command", "set print pretty on" }
+    }
+
+    dap.configurations.c = 
+    {
+      {
+        name = "launch - gdb",
+        type = "gdb",
+        request = "launch",
+        program = function ()
+          return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+        end,
+        args = {},
+        cwd = "${workspaceFolder}",
+        stopAtBeginningOfMainSubprogram = false,
+      },
+      {
+        name = "Select and attach to process",
+        type = "gdb",
+        request = "attach",
+        program = function ()
+          return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+        end,
+        pid = function ()
+          local name = vim.fn.input('Executable name (filter): ')
+          return require("dap.utils").pick_process({ filter = name })
+        end,
+        cwd = "${workspaceFolder}",
+      }
+    }
+  
+    
 
     dapui.setup({
 
