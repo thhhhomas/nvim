89474154913161d8eafe6575ed9a82a76698038d
thhhhomas <thomasferreiralopes6@gmail.com>
2026-08-02 09:59:33 -0300

    gruvbox

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -1,7 +1,10 @@
 return {
   "ellisonleao/gruvbox.nvim",
   priority = 1000,
-  config = function()
+  config = function() 
+    require("gruvbox").setup({
+      contrast = "hard",
+    })
     vim.cmd.colorscheme "gruvbox"
   end,
 }
